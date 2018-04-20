33ce483653b2e743bb363589c1dd896abf733626
JanPel <jan.pel@futurefacts.nl>
2018-04-20 15:05:56 +0200

    test

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,11 @@ FROM python:3.5.3
 WORKDIR /app/
 
 COPY requirements.txt /app/
-COPY test.html /app/templates/test.html
+COPY ./templates/index.html /app/templates/
 COPY boxer_features.p /app/
 COPY boxer_name_id.p /app/
+COPY ./website/images/background.jpg ./templates/images/
+COPY ./website/images/logo.png ./templates/images/
 RUN pip install -r ./requirements.txt
 
 COPY app.py __init__.py /app/
--- app.py
+++ app.py
@@ -19,8 +19,6 @@ def left_chance(rating_diff):
 def right_chance(rating_diff):
     chance = (1/(1+pow(10,rating_diff/350)))*(1 - drawn_chance(rating_diff))
     return chance
-    name_to_id = pickle.load( open( "boxer_name_id.p", "rb" ) )
-    features = pickle.load( open( "boxer_features.p", "rb" ) )
 name_to_id = pickle.load( open( "boxer_name_id.p", "rb" ) )
 features = pickle.load( open( "boxer_features.p", "rb" ) )
 
@@ -33,25 +31,34 @@ def index():
 
 @app.route('/prediction/api/v1.0/some_prediction', methods=['GET'])
 def get_prediction():
-    boxer1 = str(request.args.get('f1'))
-    boxer2 = str(request.args.get('f2'))
+    boxer1 = str(request.args.get('boxer_id_l'))
+    boxer1 = boxer1.replace('+',' ')
+    print(boxer1)
+    boxer2 = str(request.args.get('boxer_id_r'))
+    boxer2 = boxer2.replace('+', ' ')
+    print(boxer2)
     feature3 = str(request.args.get('f3'))
     if boxer1 in name_to_id:
         id1 = name_to_id[boxer1]['boxer_id']
     else:
-        return 'could not find '+ str(boxer1)
+        return render_template('index.html',f0=0,f1=1,f2='',f3='',f4='',f5='d')
     if boxer2 in name_to_id:
         id2 = name_to_id[boxer2]['boxer_id']
     else:
-        return 'could not find '+ str(boxer2)
+        return render_template('index.html',f0=0,
+                    f1=0,f2='',f3='',f4='',f5='d')
     rating1 = features[id1]['elo_rating']
+    print(rating1)
     rating2 = features[id2]['elo_rating']
+    print(rating2)
     difference = rating1 - rating2
     p_drawn = drawn_chance(difference)
     p_right = right_chance(difference)
+    print(p_right)
     p_left = left_chance(difference)
-    return str(p_drawn)
-#    return render_template('test.html',f1=boxer1,f2=boxer2,f3=p_drawn)
+    print(p_left)
+    return render_template('index.html',f0 =
+                           1,f1=boxer1,f2=boxer2,f3=p_drawn,f4=p_left,f5=p_right)
    
 if __name__ == '__main__':
     app.run(port=5000,host='0.0.0.0')        
