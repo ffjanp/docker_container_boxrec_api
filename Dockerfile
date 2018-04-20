c15fa892f0b07a2832423bc055b20628e20e1218
JanPel <jan.pel@futurefacts.nl>
2018-04-20 11:28:53 +0200

    addedboxer_features and and boxer_name_id

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,9 @@ FROM python:3.5.3
 WORKDIR /app/
 
 COPY requirements.txt /app/
+COPY test.html /app/templates/test.html
+COPY boxer_features.p /app/
+COPY boxer_name_id.p /app/
 RUN pip install -r ./requirements.txt
 
 COPY app.py __init__.py /app/
--- app.py
+++ app.py
@@ -4,13 +4,28 @@ from flask import request
 import pandas as pd
 from sklearn import linear_model
 import pickle
-
+import math
+from flask import render_template
 # creating and saving some model
-reg_model = linear_model.LinearRegression()
-reg_model.fit([[1.,1.,5.], [2.,2.,5.], [3.,3.,1.]], [0.,0.,1.])
-pickle.dump(reg_model, open('some_model.pkl', 'wb'))
+def drawn_chance(rating_diff):
+    sigma= 309.1541334536843
+    coefficient = 0.07355460439376074
+    chance = coefficient*math.e**(-(rating_diff**2)/(2*sigma**2))
+    return chance
+def left_chance(rating_diff):
+    chance = (1/(1+pow(10,-rating_diff/350)))*(1 - drawn_chance(rating_diff))
+    return chance
+
+def right_chance(rating_diff):
+    chance = (1/(1+pow(10,rating_diff/350)))*(1 - drawn_chance(rating_diff))
+    return chance
+    name_to_id = pickle.load( open( "boxer_name_id.p", "rb" ) )
+    features = pickle.load( open( "boxer_features.p", "rb" ) )
+name_to_id = pickle.load( open( "boxer_name_id.p", "rb" ) )
+features = pickle.load( open( "boxer_features.p", "rb" ) )
 
 app = Flask(__name__)
+
 
 @app.route('/isAlive')
 def index():
@@ -18,12 +33,25 @@ def index():
 
 @app.route('/prediction/api/v1.0/some_prediction', methods=['GET'])
 def get_prediction():
-    feature1 = float(request.args.get('f1'))
-    feature2 = float(request.args.get('f2'))
-    feature3 = float(request.args.get('f3'))
-    loaded_model = pickle.load(open('some_model.pkl', 'rb'))
-    prediction = loaded_model.predict([[feature1, feature2, feature3]])
-    return str(prediction)
+    boxer1 = str(request.args.get('f1'))
+    boxer2 = str(request.args.get('f2'))
+    feature3 = str(request.args.get('f3'))
+    if boxer1 in name_to_id:
+        id1 = name_to_id[boxer1]['boxer_id']
+    else:
+        return 'could not find '+ str(boxer1)
+    if boxer2 in name_to_id:
+        id2 = name_to_id[boxer2]['boxer_id']
+    else:
+        return 'could not find '+ str(boxer2)
+    rating1 = features[id1]['elo_rating']
+    rating2 = features[id2]['elo_rating']
+    difference = rating1 - rating2
+    p_drawn = drawn_chance(difference)
+    p_right = right_chance(difference)
+    p_left = left_chance(difference)
+    return str(p_drawn)
+#    return render_template('test.html',f1=boxer1,f2=boxer2,f3=p_drawn)
    
 if __name__ == '__main__':
     app.run(port=5000,host='0.0.0.0')        
